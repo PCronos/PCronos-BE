9d8b19668a1a05c5d22ff8bf2810bacb28d466b6
javisngo <ngotriduc0411@gmail.com>
2025-04-10 10:50:06 +0700

    create base source code

---
+++ src/test/java/com/ducnt/pcronosbe/PCronosBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ducnt.pcronosbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PCronosBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
